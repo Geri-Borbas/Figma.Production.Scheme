e613f6033a1a21ce0483c33b79788410eecb0318
Geri Borbás <geri@eppz.eu>
2021-10-27 01:40:09 +0200

    0.0.1

--- code.ts
+++ code.ts
@@ -1,38 +1,61 @@
-const selection = figma.currentPage.selection
-if (selection.length > 0 &&
-	selection[0].name.includes("Scheme") &&
-	selection[0].type === "FRAME") {
+function run() {
 
-		console.log(selection[0])
-		selection[0].children.forEach(
-			function(eachSwatch) {
-				if (eachSwatch.type === "INSTANCE") {
-					if (eachSwatch.children[0].type === "ELLIPSE" &&
-						eachSwatch.children[1].type === "TEXT") {
+	let selection = figma.currentPage.selection
 
-						// Parse style from swatch.
-						const swatchName = eachSwatch.children[1].characters
-						const swatchFills = eachSwatch.children[0].fills as [Paint]
-						console.log("swatchName: " + swatchName)
-						console.log(eachSwatch.children[0])
+	if (selection.length == 0) {
+		figma.notify("Select a Scheme first")
+		return
+	}
 
-						// Get matching style.
-						const styles = figma.getLocalPaintStyles()
-						console.log(styles)
+	if (selection[0].name.includes("Scheme") == false ||
+		selection[0].type !== "FRAME") {
+		figma.notify("Select a valid Scheme")
+		return
+	}
+	
+	let scheme = selection[0] as FrameNode
+	scheme.children.forEach(
+		function(eachSwatch) {
 
-						const style = styles.find(function(eachStyle) {
-							console.log(eachStyle)
-							console.log("eachStyle.name: " + eachStyle.name)
-							return eachStyle.name == swatchName || eachStyle.name == swatchName.replace(/\s/g, "")
-						})
-						console.log(style)
-
-						const style_ = figma.getStyleById(style.id) as PaintStyle
-						style.name = swatchName
-						style.description = "Auto-created from scheme."
-						style.paints = swatchFills
-					}
-				}
+			if (eachSwatch.type !== "INSTANCE") {
+				figma.notify("Swatches should be instances")
+				return
 			}
-		)
+
+			let ellipse = eachSwatch.children[0] as EllipseNode
+			let text = eachSwatch.children[1] as TextNode
+
+			if (ellipse.type !== "ELLIPSE" || 
+				text.type !== "TEXT") {
+				figma.notify("Swatches should contain a text and a circle")
+				return
+			}
+
+			// Parse style from swatch.
+			let styleName = text.characters
+			let stylePaints = ellipse.fills as [Paint]
+			console.log("styleName: " + styleName)
+			console.log(stylePaints)
+
+			// Get matching style if any.
+			let styles = figma.getLocalPaintStyles()
+			let style = styles.find(eachStyle =>
+				eachStyle.name == styleName || eachStyle.name == styleName.replace(/\s/g, "")
+			)
+
+			if (style === undefined) {
+				figma.notify("No matching style for `" + styleName + "`")
+				return
+			}
+
+			// Apply.
+			style.name = styleName
+			style.description = "Auto-created from scheme."
+			style.paints = stylePaints
+		}
+	)
 }
+
+run()
+figma.closePlugin()
+
