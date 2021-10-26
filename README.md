980e684bd19e453d40973d00f1718a967bce4358
Geri Borbás <geri@eppz.eu>
2021-10-27 01:05:30 +0200

    0.0.0

---
+++ README.md
@@ -0,0 +1,15 @@
+# Scheme
+
+Figma plugin to apply colors to local styles.
+
+
+## Usage
+
+1. Create a frame containing `Scheme` in the name.
+2. Add swatches (frames containing a text and a circle)
+3. Select `Scheme` and run the plugin.
+
+
+## License
+
+> Licensed under the [**MIT License**](https://en.wikipedia.org/wiki/MIT_License).
---
+++ code.js
@@ -0,0 +1,31 @@
+const selection = figma.currentPage.selection;
+if (selection.length > 0 &&
+    selection[0].name.includes("Scheme") &&
+    selection[0].type === "FRAME") {
+    console.log(selection[0]);
+    selection[0].children.forEach(function (eachSwatch) {
+        if (eachSwatch.type === "INSTANCE") {
+            if (eachSwatch.children[0].type === "ELLIPSE" &&
+                eachSwatch.children[1].type === "TEXT") {
+                // Parse style from swatch.
+                const swatchName = eachSwatch.children[1].characters;
+                const swatchFills = eachSwatch.children[0].fills;
+                console.log("swatchName: " + swatchName);
+                console.log(eachSwatch.children[0]);
+                // Get matching style.
+                const styles = figma.getLocalPaintStyles();
+                console.log(styles);
+                const style = styles.find(function (eachStyle) {
+                    console.log(eachStyle);
+                    console.log("eachStyle.name: " + eachStyle.name);
+                    return eachStyle.name == swatchName || eachStyle.name == swatchName.replace(/\s/g, "");
+                });
+                console.log(style);
+                const style_ = figma.getStyleById(style.id);
+                style.name = swatchName;
+                style.description = "Auto-created from scheme.";
+                style.paints = swatchFills;
+            }
+        }
+    });
+}
